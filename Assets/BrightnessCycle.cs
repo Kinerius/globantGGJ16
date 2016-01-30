27d3c6d99f8c60bf0bdb9c4a249d06b8d18b2b34
Kinerius <kinerius@gmail.com>
2016-01-30 16:13:47 -0300

    Merge and commit

---
+++ Assets/BrightnessCycle.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BrightnessCycle : MonoBehaviour {
+
+	public Material shader;
+	public float min = 0;
+	public float max = 1;
+	public float speedInterval = 0.1f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
